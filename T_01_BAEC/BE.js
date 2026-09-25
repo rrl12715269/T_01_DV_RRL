f304dae9ac2190dc9ea537c1ecc0c25906e3e73b
rrl12715269 <104397544@students.swinburne.edu.my>
2026-09-25 17:21:01 +0800

    Add files via upload

---
+++ T_01_BAEC/BE.js
@@ -0,0 +1,12 @@
+/////------PAGE SWITCHING--------////////
+
+function goTelevisions(){
+	window.location.href ="televisions.html";
+}
+function goHome(){
+	window.location.href ="home.html";
+}
+
+function goAboutUs(){
+	window.location.href ="aboutus.html";
+}
